385563217497f0d326f44f62f7a9431105ea6597
trgchllw <quanicey@mail.ru>
2022-05-27 16:28:33 +0300

    notworked

---
+++ producttwo.py
@@ -0,0 +1,2 @@
+NAME_TWO = "Кабель зарядный Essager 6A USB Type-C для Huawei P30/P40 Pro, 66 Вт \n 78,27 руб."
+URL_TWO = 'https://aliexpress.ru/item/1005003913701878.html?spm=a2g2w.home.15002.4.75df501dKKZTYa&_evo_buckets=165609,165598,188871,194275,299287,224373,176818&sku_id=12000027463260598&gps-id=pcJustForYou&scm=1007.13562.275840.0&scm_id=1007.13562.275840.0&scm-url=1007.13562.275840.0&pvid=b66bb616-de0a-4c3d-82ed-898deb3a6b9e&_t=gps-id:pcJustForYou,scm-url:1007.13562.275840.0,pvid:b66bb616-de0a-4c3d-82ed-898deb3a6b9e,tpp_buckets:21387%230%23250274%230_21387%239507%23434562%237'
